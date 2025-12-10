d58335cd496ca71f51698304bd6d3fae9f00df46
Tilcob <jakobdorninger19@gmail.com>
2025-12-10 19:57:50 +0100

    Changed from float to double for better performance and accuracy.

--- core/src/main/java/com/physics/math/ComplexNumber.java
+++ core/src/main/java/com/physics/math/ComplexNumber.java
@@ -1,10 +1,10 @@
 package com.physics.math;
 
 public class ComplexNumber {
-    private float real;
-    private float imag;
+    private double real;
+    private double imag;
 
-    public ComplexNumber(float real, float imag) {
+    public ComplexNumber(double real, double imag) {
         this.real = real;
         this.imag = imag;
     }
@@ -13,30 +13,30 @@ public class ComplexNumber {
         this.imag = 0;
     }
 
-    public void add(float a, float b) {
+    public void add(double a, double b) {
         this.real += a;
         this.imag += b;
     }
 
-    public void subtract(float a, float b) {
+    public void subtract(double a, double b) {
         this.real -= a;
         this.imag -= b;
     }
 
     public void multiply(ComplexNumber c) {
-        float newReal = this.real * c.real - this.imag * c.imag;
-        float newImag = this.real * c.imag + this.imag * c.real;
+        double newReal = this.real * c.real - this.imag * c.imag;
+        double newImag = this.real * c.imag + this.imag * c.real;
         this.real = newReal;
         this.imag = newImag;
     }
 
     public void divide(ComplexNumber c) {
-        float denom = c.real * c.real + c.imag * c.imag;
+        double denom = c.real * c.real + c.imag * c.imag;
         if (denom == 0) {
             throw new ArithmeticException("Division durch Null");
         }
-        float newReal = (this.real * c.real + this.imag * c.imag) / denom;
-        float newImag = (this.imag * c.real - this.real * c.imag) / denom;
+        double newReal = (this.real * c.real + this.imag * c.imag) / denom;
+        double newImag = (this.imag * c.real - this.real * c.imag) / denom;
         this.real = newReal;
         this.imag = newImag;
     }
@@ -45,19 +45,19 @@ public class ComplexNumber {
         return new ComplexNumber(real, -imag);
     }
 
-    public float getReal() {
+    public double getReal() {
         return real;
     }
 
-    public void setReal(float real) {
+    public void setReal(double real) {
         this.real = real;
     }
 
-    public float getImag() {
+    public double getImag() {
         return imag;
     }
 
-    public void setImag(float imag) {
+    public void setImag(double imag) {
         this.imag = imag;
     }
 
--- core/src/main/java/com/physics/math/LinearAlgebra.java
+++ core/src/main/java/com/physics/math/LinearAlgebra.java
@@ -3,44 +3,42 @@ package com.physics.math;
 import com.physics.math.interfaces.Function2;
 import com.physics.math.interfaces.Function3;
 import org.joml.*;
-
-import java.lang.Math;
 import java.util.function.Function;
 
 import static com.physics.config.Constants.*;
 
 public class LinearAlgebra {
-    private static final float h = DEFAULT_H;
+    private static final double h = DEFAULT_H;
 
     private LinearAlgebra() {}
 
     // Derivatives
 
-    public static float derivative(float x, Function<Float, Float> function) {
+    public static double derivative(double x, Function<Double, Double> function) {
         return (function.apply(x + h) - function.apply(x - h)) / (2 * h);
     }
 
-    public static float partialDerivativeX(float x, float y, float z, Function3<Float, Float, Float, Float> function) {
+    public static double partialDerivativeX(double x, double y, double z, Function3<Double, Double, Double, Double> function) {
         return (function.apply(x + h, y, z) - function.apply(x - h, y, z)) / (2 * h);
     }
-    public static float partialDerivativeX(float x, float y, Function2<Float, Float, Float> function) {
+    public static double partialDerivativeX(double x, double y, Function2<Double, Double, Double> function) {
         return (function.apply(x + h, y) - function.apply(x - h, y)) / (2 * h);
     }
 
-    public static float partialDerivativeY(float x, float y, float z, Function3<Float, Float, Float, Float> function) {
+    public static double partialDerivativeY(double x, double y, double z, Function3<Double, Double, Double, Double> function) {
         return (function.apply(x, y + h, z) - function.apply(x, y - h, z)) / (2 * h);
     }
-    public static float partialDerivativeY(float x, float y, Function2<Float, Float, Float> function) {
+    public static double partialDerivativeY(double x, double y, Function2<Double, Double, Double> function) {
         return (function.apply(x, y + h) - function.apply(x, y - h)) / (2 * h);
     }
 
-    public static float partialDerivativeZ(float x, float y, float z, Function3<Float, Float, Float, Float> function) {
+    public static double partialDerivativeZ(double x, double y, double z, Function3<Double, Double, Double, Double> function) {
         return (function.apply(x, y, z + h) - function.apply(x, y, z - h)) / (2 * h);
     }
 
-    public static float partialDerivative(float[] point, int dimension, Function<float[], Float> function) {
-        float[] forward = point.clone();
-        float[] backward = point.clone();
+    public static double partialDerivative(double[] point, int dimension, Function<double[], Double> function) {
+        double[] forward = point.clone();
+        double[] backward = point.clone();
 
         forward[dimension] += h;
         backward[dimension] -= h;
@@ -48,9 +46,9 @@ public class LinearAlgebra {
         return (function.apply(forward) - function.apply(backward)) / (2 * h);
     }
 
-    public static float partialDerivativeVec2(float[] point, int dimension, Function<Vector2f, Float> function) {
-        Vector2f forward = new Vector2f(point);
-        Vector2f backward = new Vector2f(point);
+    public static double partialDerivativeVec2(double[] point, int dimension, Function<Vector2d, Double> function) {
+        Vector2d forward = new Vector2d(point);
+        Vector2d backward = new Vector2d(point);
 
         switch (dimension) {
             case 0 -> {
@@ -67,9 +65,9 @@ public class LinearAlgebra {
         return (function.apply(forward) - function.apply(backward)) / (2 * h);
     }
 
-    public static float partialDerivativeVec3(float[] point, int dimension, Function<Vector3f, Float> function) {
-        Vector3f forward = new Vector3f(point);
-        Vector3f backward = new Vector3f(point);
+    public static double partialDerivativeVec3(double[] point, int dimension, Function<Vector3d, Double> function) {
+        Vector3d forward = new Vector3d(point);
+        Vector3d backward = new Vector3d(point);
 
         switch (dimension) {
             case 0 -> {
@@ -92,32 +90,32 @@ public class LinearAlgebra {
 
     // Gradients
 
-    public static float[] gradient(float[] p, Function<float[], Float> f) {
-        float[] g = new float[p.length];
+    public static double[] gradient(double[] p, Function<double[], Double> f) {
+        double[] g = new double[p.length];
         for (int i = 0; i < p.length; i++) {
             g[i] = partialDerivative(p, i, f);
         }
         return g;
     }
 
-    public static Vector2f gradient(float x, float y, Function2<Float, Float, Float> function) {
-        return new Vector2f(
+    public static Vector2d gradient(double x, double y, Function2<Double, Double, Double> function) {
+        return new Vector2d(
             partialDerivativeX(x, y, function),
             partialDerivativeY(x, y, function)
         );
     }
 
-    public static Vector3f gradient(float x, float y, float z, Function3<Float, Float, Float, Float> function) {
-        return new Vector3f(
+    public static Vector3d gradient(double x, double y, double z, Function3<Double, Double, Double, Double> function) {
+        return new Vector3d(
             partialDerivativeX(x, y, z, function),
             partialDerivativeY(x, y, z, function),
             partialDerivativeZ(x, y, z, function)
         );
     }
 
-    public static Vector4f gradient(float x, float y, float z, float w, Function<float[], Float> function) {
-        float[] gradient = gradient(new float[]{x,y,z,w}, function);
-        return new Vector4f(
+    public static Vector4d gradient(double x, double y, double z, double w, Function<double[], Double> function) {
+        double[] gradient = gradient(new double[]{x,y,z,w}, function);
+        return new Vector4d(
             gradient[0],
             gradient[1],
             gradient[2],
@@ -127,13 +125,13 @@ public class LinearAlgebra {
 
     // Hesse-Matrix
 
-    public static Matrix2f hessian(Vector2f vector, Function<Vector2f, Float> function) {
-        float[] point = {vector.x, vector.y};
+    public static Matrix2d hessian(Vector2d vector, Function<Vector2d, Double> function) {
+        double[] point = {vector.x, vector.y};
 
-        Matrix2f hessian = new Matrix2f();
+        Matrix2d hessian = new Matrix2d();
 
-        Function<Vector2f, Float> partialF1 = p -> partialDerivativeVec2(new float[]{p.x, p.y}, 0, function);
-        Function<Vector2f, Float> partialF2 = p -> partialDerivativeVec2(new float[]{p.x, p.y}, 1, function);
+        Function<Vector2d, Double> partialF1 = p -> partialDerivativeVec2(new double[]{p.x, p.y}, 0, function);
+        Function<Vector2d, Double> partialF2 = p -> partialDerivativeVec2(new double[]{p.x, p.y}, 1, function);
 
         hessian.m00 = partialDerivativeVec2(point, 0, partialF1);
         hessian.m01 = partialDerivativeVec2(point, 1, partialF1);
@@ -142,13 +140,13 @@ public class LinearAlgebra {
         return hessian;
     }
 
-    public static Matrix3f hessian(Vector3f vector, Function<Vector3f, Float> function) {
-        float[] point = {vector.x, vector.y, vector.z};
-        Matrix3f hessian = new Matrix3f();
+    public static Matrix3d hessian(Vector3d vector, Function<Vector3d, Double> function) {
+        double[] point = {vector.x, vector.y, vector.z};
+        Matrix3d hessian = new Matrix3d();
 
-        Function<Vector3f, Float> partialF1 = p -> partialDerivativeVec3(new float[]{p.x, p.y, p.z}, 0, function);
-        Function<Vector3f, Float> partialF2 = p -> partialDerivativeVec3(new float[]{p.x, p.y, p.z}, 1, function);
-        Function<Vector3f, Float> partialF3 = p -> partialDerivativeVec3(new float[]{p.x, p.y, p.z}, 2, function);
+        Function<Vector3d, Double> partialF1 = p -> partialDerivativeVec3(new double[]{p.x, p.y, p.z}, 0, function);
+        Function<Vector3d, Double> partialF2 = p -> partialDerivativeVec3(new double[]{p.x, p.y, p.z}, 1, function);
+        Function<Vector3d, Double> partialF3 = p -> partialDerivativeVec3(new double[]{p.x, p.y, p.z}, 2, function);
 
         hessian.m00 = partialDerivativeVec3(point, 0, partialF1);
         hessian.m01 = partialDerivativeVec3(point, 1, partialF1);
@@ -163,13 +161,13 @@ public class LinearAlgebra {
         return hessian;
     }
 
-    public static float[][] hessian(float[] point, Function<float[], Float> function) {
+    public static double[][] hessian(double[] point, Function<double[], Double> function) {
         int n = point.length;
-        float[][] hessian = new float[n][n];
+        double[][] hessian = new double[n][n];
 
         for (int i = 0; i < n; i++) {
             int finalI = i;
-            Function<float[], Float> partialFi = p -> partialDerivative(p, finalI, function);
+            Function<double[], Double> partialFi = p -> partialDerivative(p, finalI, function);
 
             for (int j = 0; j < n; j++) {
                 hessian[i][j] = partialDerivative(point, j, partialFi);
@@ -181,18 +179,18 @@ public class LinearAlgebra {
 
     // Jacobian-Matrix
 
-    public static Vector2f jacobianVec2(Function<Float, Vector2f> function, float q) {
-        float dx_dq = derivative(q, (dx) -> function.apply(dx).x);
-        float dy_dq = derivative(q, (dy) -> function.apply(dy).y);
-        return new Vector2f(dx_dq, dy_dq);
+    public static Vector2d jacobianVec2(Function<Double, Vector2d> function, double q) {
+        double dx_dq = derivative(q, (dx) -> function.apply(dx).x);
+        double dy_dq = derivative(q, (dy) -> function.apply(dy).y);
+        return new Vector2d(dx_dq, dy_dq);
     }
 
-    public static Matrix2f jacobianMat2(Function<Vector2f, Vector2f> function, Vector2f q) {
-        Matrix2f jacobian = new Matrix2f();
-        float[] point = {q.x, q.y};
+    public static Matrix2d jacobianMat2(Function<Vector2d, Vector2d> function, Vector2d q) {
+        Matrix2d jacobian = new Matrix2d();
+        double[] point = {q.x, q.y};
 
-        Function<Vector2f, Float> f1 = (v) -> function.apply(v).x;
-        Function<Vector2f, Float> f2 = (v) -> function.apply(v).y;
+        Function<Vector2d, Double> f1 = (v) -> function.apply(v).x;
+        Function<Vector2d, Double> f2 = (v) -> function.apply(v).y;
 
         jacobian.m00 = partialDerivativeVec2(point, 0, f1);
         jacobian.m01 = partialDerivativeVec2(point, 1, f1);
@@ -202,21 +200,21 @@ public class LinearAlgebra {
         return jacobian;
     }
 
-    public static Vector3f jacobianVec3(Function<Float, Vector3f> function, float q) {
-        float dx_dq = derivative(q, (dx) -> function.apply(dx).x);
-        float dy_dq = derivative(q, (dy) -> function.apply(dy).y);
-        float dz_dq = derivative(q, (dz) -> function.apply(dz).z);
-        return new Vector3f(dx_dq, dy_dq, dz_dq);
+    public static Vector3d jacobianVec3(Function<Double, Vector3d> function, double q) {
+        double dx_dq = derivative(q, (dx) -> function.apply(dx).x);
+        double dy_dq = derivative(q, (dy) -> function.apply(dy).y);
+        double dz_dq = derivative(q, (dz) -> function.apply(dz).z);
+        return new Vector3d(dx_dq, dy_dq, dz_dq);
     }
 
-    public static Matrix3x2f jacobianMat3x2(Function<Vector2f, Vector3f> function, Vector2f q) {
-        Matrix3x2f jacobian = new Matrix3x2f();
-        float[] point = {q.x, q.y};
+    public static Matrix3x2d jacobianMat3x2(Function<Vector2d, Vector3d> function, Vector2d q) {
+        Matrix3x2d jacobian = new Matrix3x2d();
+        double[] point = {q.x, q.y};
 
 
-        Function<Vector2f, Float> f1 = (v) -> function.apply(v).x;
-        Function<Vector2f, Float> f2 = (v) -> function.apply(v).y;
-        Function<Vector2f, Float> f3 = (v) -> function.apply(v).z;
+        Function<Vector2d, Double> f1 = (v) -> function.apply(v).x;
+        Function<Vector2d, Double> f2 = (v) -> function.apply(v).y;
+        Function<Vector2d, Double> f3 = (v) -> function.apply(v).z;
 
         jacobian.m00 = partialDerivativeVec2(point, 0, f1);
         jacobian.m01 = partialDerivativeVec2(point, 1, f1);
@@ -228,13 +226,13 @@ public class LinearAlgebra {
         return jacobian;
     }
 
-    public static Matrix3f jacobianMat3X3(Function<Vector3f, Vector3f> function, Vector3f q) {
-        Matrix3f jacobian = new Matrix3f();
-        float[] point = {q.x, q.y, q.z};
+    public static Matrix3d jacobianMat3X3(Function<Vector3d, Vector3d> function, Vector3d q) {
+        Matrix3d jacobian = new Matrix3d();
+        double[] point = {q.x, q.y, q.z};
 
-        Function<Vector3f, Float> f1 = (v) -> function.apply(v).x;
-        Function<Vector3f, Float> f2 = (v) -> function.apply(v).y;
-        Function<Vector3f, Float> f3 = (v) -> function.apply(v).z;
+        Function<Vector3d, Double> f1 = (v) -> function.apply(v).x;
+        Function<Vector3d, Double> f2 = (v) -> function.apply(v).y;
+        Function<Vector3d, Double> f3 = (v) -> function.apply(v).z;
 
         jacobian.m00 = partialDerivativeVec3(point, 0, f1);
         jacobian.m01 = partialDerivativeVec3(point, 1, f1);
@@ -249,11 +247,11 @@ public class LinearAlgebra {
         return jacobian;
     }
 
-    public static float[][] jacobian(float[] point, Function<float[], Float>[] functions) {
-        float[][] jacobian = new  float[functions.length][point.length];
+    public static double[][] jacobian(double[] point, Function<double[], Double>[] functions) {
+        double[][] jacobian = new  double[functions.length][point.length];
 
         for (int i = 0; i < functions.length; i++) {
-            float[] gradient = gradient(point, functions[i]);
+            double[] gradient = gradient(point, functions[i]);
             jacobian[i] = gradient;
         }
         return jacobian;
@@ -261,56 +259,56 @@ public class LinearAlgebra {
 
     // Integration
 
-    public static float integrate(Function<Float, Float> function, float lowerLimit, float upperLimit) {
-        float xm = .5f * (upperLimit + lowerLimit);
-        float xr = .5f * (upperLimit - lowerLimit);
-        float sum = 0;
+    public static double integrate(Function<Double, Double> function, double lowerLimit, double upperLimit) {
+        double xm = .5f * (upperLimit + lowerLimit);
+        double xr = .5f * (upperLimit - lowerLimit);
+        double sum = 0;
 
         for (int i = 0; i < 3; i++) {
-            float dx = xr * ABSCISSA[i];
+            double dx = xr * ABSCISSA[i];
             sum += WEIGHT[i] * function.apply(xm + dx);
         }
 
         return sum * xr;
     }
 
-    public static float integral(Function2<Float, Float, Float> function,
-                                 float lowerLimitX, float upperLimitX,
-                                 float lowerLimitY, float upperLimitY) {
-        float xm = .5f * (upperLimitX + lowerLimitX);
-        float xr = .5f * (upperLimitX - lowerLimitX);
-        float ym = .5f * (upperLimitY + lowerLimitY);
-        float yr = .5f * (upperLimitY - lowerLimitY);
-        float sum = 0;
+    public static double integral(Function2<Double, Double, Double> function,
+                                 double lowerLimitX, double upperLimitX,
+                                 double lowerLimitY, double upperLimitY) {
+        double xm = .5f * (upperLimitX + lowerLimitX);
+        double xr = .5f * (upperLimitX - lowerLimitX);
+        double ym = .5f * (upperLimitY + lowerLimitY);
+        double yr = .5f * (upperLimitY - lowerLimitY);
+        double sum = 0;
 
         for (int i = 0; i < 3; i++) {
-            float dx = xr * ABSCISSA[i];
+            double dx = xr * ABSCISSA[i];
             for (int j = 0; j < 3; j++) {
-                float dy = yr * ABSCISSA[j];
+                double dy = yr * ABSCISSA[j];
                 sum += WEIGHT[i] * WEIGHT[j] * function.apply(xm + dx, ym +dy);
             }
         }
         return sum * xr * yr;
     }
 
-    public static float integral(Function3<Float, Float, Float, Float> function,
-                                 float lowerLimitX, float upperLimitX,
-                                 float lowerLimitY, float upperLimitY,
-                                 float lowerLimitZ, float upperLimitZ) {
-        float xm = .5f * (upperLimitX + lowerLimitX);
-        float xr = .5f * (upperLimitX - lowerLimitX);
-        float ym = .5f * (upperLimitY + lowerLimitY);
-        float yr = .5f * (upperLimitY - lowerLimitY);
-        float zm = .5f * (upperLimitZ + lowerLimitZ);
-        float zr = .5f * (upperLimitZ - lowerLimitZ);
-        float sum = 0;
+    public static double integral(Function3<Double, Double, Double, Double> function,
+                                 double lowerLimitX, double upperLimitX,
+                                 double lowerLimitY, double upperLimitY,
+                                 double lowerLimitZ, double upperLimitZ) {
+        double xm = .5f * (upperLimitX + lowerLimitX);
+        double xr = .5f * (upperLimitX - lowerLimitX);
+        double ym = .5f * (upperLimitY + lowerLimitY);
+        double yr = .5f * (upperLimitY - lowerLimitY);
+        double zm = .5f * (upperLimitZ + lowerLimitZ);
+        double zr = .5f * (upperLimitZ - lowerLimitZ);
+        double sum = 0;
 
         for (int i = 0; i < 3; i++) {
-            float dx = xr * ABSCISSA[i];
+            double dx = xr * ABSCISSA[i];
             for (int j = 0; j < 3; j++) {
-                float dy = yr * ABSCISSA[j];
+                double dy = yr * ABSCISSA[j];
                 for (int k = 0; k < 3; k++) {
-                    float dz = zr * ABSCISSA[k];
+                    double dz = zr * ABSCISSA[k];
                     sum += WEIGHT[i] * WEIGHT[j] * WEIGHT[k] * function.apply(xm + dx, ym + dy, zm + dz);
                 }
             }
@@ -320,17 +318,17 @@ public class LinearAlgebra {
 
     // Utils
 
-    public static float[][] multiply(float[][] A, float[][] B) {
+    public static double[][] multiply(double[][] A, double[][] B) {
         int m = A.length;
         int n = A[0].length;
         int p = B[0].length;
 
         if (B.length != n) throw new IllegalArgumentException("Dimension mismatch");
 
-        float[][] C = new float[m][p];
+        double[][] C = new double[m][p];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < p; j++) {
-                float sum = 0;
+                double sum = 0;
                 for (int k = 0; k < n; k++) {
                     sum += A[i][k] * B[k][j];
                 }
@@ -340,11 +338,11 @@ public class LinearAlgebra {
         return C;
     }
 
-    public static float[][] transpose(float[][] matrix) {
+    public static double[][] transpose(double[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        float[][] transposed = new float[n][m];
+        double[][] transposed = new double[n][m];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
